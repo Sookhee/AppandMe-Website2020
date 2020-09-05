a6ed08f3cec1b7b4e509aa82c949429a175ecbef
Jeong Minji <s2018s38@e-mirim.hs.kr>
2020-09-05 19:58:34 +0900

    반응형 Apply 완성

--- client/src/component/Apply.js
+++ client/src/component/Apply.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import '../style/Apply.scss';
 import Titlebar from './Titlebar';
 
 const Apply = () => {
@@ -10,7 +11,40 @@ const Apply = () => {
                     note1={"앱앤미 지원을 결심하셨군요! 좋은 결과 있길 바랍니다 :)"}
                     note2={"* 제출 후 수정이 불가하니 신중히 작성해주세요."
                 }/>
-                APPLY
+                
+                <form className="apply-form">
+                    <table>
+                        <tr>
+                            <td>학번</td>
+                            <td className="temp"></td>
+                            <td>이름</td>
+                        </tr>
+                        <tr>
+                            <td><input type="number" name="sid" placeholder="학번을 입력해주세요"/></td>
+                            <td className="temp"></td>
+                            <td><input type="text" name="name" placeholder="이름을 입력해주세요"/></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="3">자기소개</td>
+                        </tr>
+                        <tr>
+                            <td colSpan="3"><textarea name="q1" placeholder="간단히 자신을 소개해주세요(최대 200자)"></textarea></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="3">지원동기</td>
+                        </tr>
+                        <tr>
+                            <td colSpan="3"><textarea name="q2" placeholder="앱앤미에 지원하게 된 동기를 알려주세요(최대 200자)"></textarea></td>
+                        </tr>
+                        <tr>
+                            <td colSpan="3">다섯글자</td>
+                        </tr>
+                        <tr>
+                            <td colSpan="3"><input type="text" name="q3" placeholder="다섯글자로 나를 표현한다면?"/></td>
+                        </tr>
+                    </table>
+                    <button type="submit" className="btn btn-pink fw-bold">SUBMIT</button>
+                </form>
             </div>
         </div>
     );
