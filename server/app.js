62f642cb3ba38f1304ea89200866a2f387c8e5df
Jeong Minji <s2018s38@e-mirim.hs.kr>
2020-09-06 23:54:20 +0900

    Navbar Desktop 버전 navitem 간격 조절

--- server/app.js
+++ server/app.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
+const port = 5000;
 
 app.get('/', (req, res) => {
     res.send('Hello');
 })
 
-const port = 5000;
 app.listen(port, () => {
     console.log(`${port} port is Running`)
 })
