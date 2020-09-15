a7891025b77da61797655bbe1c9683af5399930c
Jeong Minji <s2018s38@e-mirim.hs.kr>
2020-09-15 09:24:48 +0900

    반응형 Notfound 페이지 완성

--- client/src/component/NotFound.js
+++ client/src/component/NotFound.js
@@ -1,10 +1,18 @@
 import React from 'react';
+import {NavLink} from 'react-router-dom';
+import '../style/NotFound.scss';
 
 const NotFound = () => {
     return (
-        <div className="notfound">
+        <div>
             <div className="container">
-                NotFound
+                <div className="notfound">
+                    <h2>존재하지 않는 페이지 입니다.</h2>
+                    <p>궁금하신 사항은 앱앤미 페이스북 페이지로 문의해 주시기 바랍니다.</p>
+                    <NavLink to="/">
+                        <button className="btn btn-pink fw-bold">홈으로</button>
+                    </NavLink>
+                </div>
             </div>
         </div>
     );
