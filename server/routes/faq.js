e1731126b3a9f68ed49579ee063d8f66cb2b4086
Jeong Minji <s2018s38@e-mirim.hs.kr>
2020-09-20 10:24:31 +0900

    server faq route 정리

--- server/routes/faq.js
+++ server/routes/faq.js
@@ -1,8 +1,13 @@
 const express = require('express');
 const router = express.Router();
+const connection = require('../db/db_conn');
+const sql = require('../db/sql');
 
 router.get('/', (req, res) => {
-    res.send('hello faq');
+    connection.query(sql.faq.select_all, [], (err, result) => {
+        if(err){ res.json({success: false, err: err}); }
+        else{ res.json({success: true, data: result}); }
+    })
 })
 
 module.exports = router;
