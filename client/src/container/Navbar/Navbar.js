86ec253b6e5be58535452daf7fe0a5ddae379dc3
sookhee <s2018s38@e-mirim.hs.kr>
2020-10-24 04:15:30 +0900

    navbar activestyle 수정

--- client/src/container/Navbar/Navbar.js
+++ client/src/container/Navbar/Navbar.js
@@ -6,12 +6,15 @@ import IconDropFalse from '../../image/navbar/icon_drop-false.png';
 
 const Navbar = () => {
     const [isDrop, setIsDrop] = useState(false);
-    const toggleIsDropHandler = () => {
-        setIsDrop(!isDrop);
-    }
-    const setIsDropFalse = () => {
-        setIsDrop(false);
-    }
+    const toggleIsDropHandler = () => {setIsDrop(!isDrop);}
+    const setIsDropFalse = () => {setIsDrop(false);}
+
+    const [isHome, setIsHome] = useState(true);
+    const setIsHomeTrue = () => {setIsHome(true)}
+    const setIsHomeFalse = () => {setIsHome(false)}
+
+    const setBoth = () => {setIsHome(true); setIsDrop(false);}
+    const setBothFalse = () => {setIsHome(false); setIsDrop(false);}
 
     return (
         <div className="navbar">
@@ -19,14 +22,14 @@ const Navbar = () => {
                 <div className="wrap-navbar">
                     <div className="nav-brand">
                         <div className="nav-logo"></div>
-                        <NavLink to="/" className="fw-bold" onClick={setIsDropFalse}>APP&#38;ME</NavLink>
+                        <NavLink to="/" onClick={setBoth}>APP&#38;ME</NavLink>
                     </div>
                     <div className="nav-menu-pc">
-                        <NavLink to="/" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}}>ABOUT</NavLink>
-                        <NavLink to="/members" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}}>MEMBERS</NavLink>
-                        <NavLink to="/apply" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}}>APPLY</NavLink>
-                        <NavLink to="/mypage" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}}>MYPAGE</NavLink>
-                        <NavLink to="/faq" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}}>FAQ</NavLink>
+                        <NavLink to="/" className="nav-item" onClick={setIsHomeTrue} style={isHome ? {color: '#ffb0b8'} : {color: '#707070'}}>ABOUT</NavLink>
+                        <NavLink to="/members" className="nav-item" onClick={setIsHomeFalse} activeStyle={{color: '#ffb0b8'}}>MEMBERS</NavLink>
+                        <NavLink to="/apply" className="nav-item" onClick={setIsHomeFalse} activeStyle={{color: '#ffb0b8'}}>APPLY</NavLink>
+                        <NavLink to="/mypage" className="nav-item" onClick={setIsHomeFalse} activeStyle={{color: '#ffb0b8'}}>MYPAGE</NavLink>
+                        <NavLink to="/faq" className="nav-item" onClick={setIsHomeFalse} activeStyle={{color: '#ffb0b8'}}>FAQ</NavLink>
                     </div>
                     <div className="nav-menu-mobile">
                         <div className="toggle-button" onClick={toggleIsDropHandler} style={isDrop ? {backgroundImage: `url(${IconDropTrue})`} : {backgroundImage: `url(${IconDropFalse})`}}></div>
@@ -35,11 +38,11 @@ const Navbar = () => {
             </div>
             <div className="container">
                 <div className="nav-drop" style={isDrop ? {display: 'flex'} : {display: 'none'}}>
-                    <NavLink to="/" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}} onClick={setIsDropFalse}>ABOUT</NavLink>
-                    <NavLink to="/members" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}} onClick={setIsDropFalse}>MEMBERS</NavLink>
-                    <NavLink to="/apply" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}} onClick={setIsDropFalse}>APPLY</NavLink>
-                    <NavLink to="/mypage" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}} onClick={setIsDropFalse}>MYPAGE</NavLink>
-                    <NavLink to="/faq" className="nav-item fw-medium" activeStyle={{color: '#ffb0b8'}} onClick={setIsDropFalse}>FAQ</NavLink>
+                    <NavLink to="/" className="nav-item" onClick={setIsHomeTrue} style={isHome ? {color: '#ffb0b8'} : {color: '#707070'}} onClick={setIsDropFalse}>ABOUT</NavLink>
+                    <NavLink to="/members" className="nav-item" onClick={setBothFalse} activeStyle={{color: '#ffb0b8'}}>MEMBERS</NavLink>
+                    <NavLink to="/apply" className="nav-item" onClick={setBothFalse} activeStyle={{color: '#ffb0b8'}}>APPLY</NavLink>
+                    <NavLink to="/mypage" className="nav-item" onClick={setBothFalse} activeStyle={{color: '#ffb0b8'}}>MYPAGE</NavLink>
+                    <NavLink to="/faq" className="nav-item" onClick={setBothFalse} activeStyle={{color: '#ffb0b8'}}>FAQ</NavLink>
                 </div>
             </div>
             <hr/>
